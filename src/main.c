c88eff54991be8ece08cf0966205251e89af0e67
Matheus Cirillo <matheuscirillo@usp.br>
2022-06-29 16:46:03 -0300

    memory leaks fixed

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "exercicio.h"
 #include "utils.h"
@@ -9,7 +10,7 @@ int main(){
     int* consultas_numericas_original = ler_inteiros("res/inteiros_busca.txt", N);
 
     printf("**Busca sequencial simples** \n");
-    ex1a(0, entradas_numericas_original, consultas_numericas_original);
+    ex1a(1, entradas_numericas_original, consultas_numericas_original);
     printf("\n\n\n");
 
     printf("**Busca mover-para-frente** \n");
@@ -24,10 +25,20 @@ int main(){
     ex1d(0, entradas_numericas_original, consultas_numericas_original);
     printf("\n\n\n");
 
+    free(entradas_numericas_original);
+    free(consultas_numericas_original);
+
     string* insercoes_string_original = ler_strings("res/strings_entrada.txt", INPUTSIZE);
     string* consultas_string_original = ler_strings("res/strings_busca.txt", CONSULTASIZE);
 
     printf("**Busca hashing aberto** \n");
-    ex2c(1, insercoes_string_original, consultas_string_original);
+    ex2c(0, insercoes_string_original, consultas_string_original);
     printf("\n\n\n");
+
+    for(int i = 0; i < INPUTSIZE; i++)
+        free(insercoes_string_original[i]);
+    free(insercoes_string_original);
+    for(int i = 0; i < CONSULTASIZE; i++)
+        free(consultas_string_original[i]);
+    free(consultas_string_original);
 }
