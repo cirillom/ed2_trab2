1f4c02316473c3f634178d4f4162f895cbe3c7d3
Matheus Cirillo <matheuscirillo@usp.br>
2022-06-30 14:49:33 -0300

    hashSearcherTest prototype ex2c not cleaned

--- src/exercicio2c.c
+++ src/exercicio2c.c
@@ -20,6 +20,79 @@ unsigned h_mul(string str, unsigned B){
     return fmod(x * A, 1) * B;
 }
 
+unsigned h_improved(string s, unsigned B) {
+   unsigned h = 1;
+   const unsigned p1 = 189437;
+
+   for (int i = 0; s[i] != '\0'; i++) 
+      h = (h * s[i] * p1)%B;
+   return h;
+}
+
+void showHashTable(Node **hashTable, string fileLocation){
+    FILE* fp = fopen(fileLocation, "a");
+    for (int i = 0; i < INPUTSIZE; i++){
+        printList(&hashTable[i], fp);
+        fprintf(fp, "\n");
+    }
+    fclose(fp);
+}
+
+unsigned hashSearcherTest(unsigned (*hash_function)(string, unsigned), string* insercoes, string* consultas, string identifier){
+    const unsigned B = 150001;
+    unsigned encontrados = 0;
+    unsigned colisoes = 0;
+
+    Node** hashTable = (Node**) malloc(sizeof(Node*) * B);
+    for (int i = 0; i < B; i++){
+        hashTable[i] = NULL;
+    }
+    
+    // inserção dos dados na tabela hash com hash por divisão        
+    clock_t _ini = inicia_tempo();
+    for (int i = 0; i < INPUTSIZE; i++) {
+        // inserir insercoes[i] na tabela hash
+        unsigned key = hash_function(insercoes[i], B);
+        if(hashTable[key] != NULL)
+            colisoes++;
+        insert(&hashTable[key], insercoes[i]);
+    }
+    double tempo_insercao = finaliza_tempo(_ini);
+
+    //mostra a hash table em um arquivo
+    string fileLoc = "build/hash_table_";
+    string fileExtension = ".txt";
+    strcat(fileLoc, identifier);
+    strcat(fileLoc, fileExtension);
+    showHashTable(hashTable, fileLoc);
+    
+    // busca dos dados na tabela hash com hash por divisão
+    _ini = inicia_tempo();
+    for (int i = 0; i < CONSULTASIZE; i++) {
+        // buscar consultas[i] na tabela hash
+        unsigned key = hash_function(consultas[i], B);
+
+        if(hashTable[key] == NULL)
+            continue;
+        
+        if(findInList(&hashTable[key], consultas[i]) != NULL)
+            encontrados++;
+    }
+    double tempo_busca = finaliza_tempo(_ini);
+
+    // destroi tabela hash com hash por divisão
+    for(int i = 0; i < B; i++)
+        freeList(&hashTable[i]);
+    free(hashTable);
+
+    printf("Hash por %s \t-> ", identifier);
+    printf("Colisões na inserção: %d | ", colisoes);
+    printf("Tempo de inserção: %fs | ", tempo_insercao);
+    printf("Tempo de busca: %fs | ", tempo_busca);
+    printf("Itens encontrados: %d", encontrados);
+
+}
+
 int ex2c(int n_testes, string* insercao_original, string* consultas_original){
     for(int j = 0; j < n_testes; j++){
         const int B = 150001;
