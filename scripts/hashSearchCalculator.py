cf97a530db3f2b73bf534f3cf29cb62ff0fa45db
Matheus Cirillo <matheuscirillo@usp.br>
2022-07-05 12:38:57 -0300

    search and insertion mean graphs created

--- scripts/hashSearchCalculator.py
+++ scripts/hashSearchCalculator.py
@@ -23,70 +23,104 @@ dfs_busca_pri = [
     pd.read_csv(r'./out/busca_hash_aberto_primos.csv', usecols=[3]),
     pd.read_csv(r'./out/busca_hash_aberto_primos.csv', usecols=[3])]
 
+dfs_insercao_div = [
+    pd.read_csv(r'./out/busca_hash_aberto_divisao.csv', usecols=[2]),
+    pd.read_csv(r'./out/busca_hash_aberto_divisao.csv', usecols=[2]),
+    pd.read_csv(r'./out/busca_hash_aberto_divisao.csv', usecols=[2])]
+
+dfs_insercao_mul = [
+    pd.read_csv(r'./out/busca_hash_aberto_multiplicacao.csv', usecols=[2]),
+    pd.read_csv(r'./out/busca_hash_aberto_multiplicacao.csv', usecols=[2]),
+    pd.read_csv(r'./out/busca_hash_aberto_multiplicacao.csv', usecols=[2])]
+
+dfs_insercao_pri = [
+    pd.read_csv(r'./out/busca_hash_aberto_primos.csv', usecols=[2]),
+    pd.read_csv(r'./out/busca_hash_aberto_primos.csv', usecols=[2]),
+    pd.read_csv(r'./out/busca_hash_aberto_primos.csv', usecols=[2])]
+
+dfs_busca = [dfs_busca_div, dfs_busca_mul, dfs_busca_pri]
+dfs_insercao = [dfs_busca_div, dfs_insercao_mul, dfs_insercao_pri]
 
 df_types = [
     'Progressive Overflow',
     'Duplo',
-    'Aberto'
-]
+    'Aberto']
 
 df_names = [
     'Divisão',
     'Multiplicação',
-    'Primo'
-]
+    'Primo']
 
 def dataframeCleaner(df, name):
     df.sort_values(df.keys()[0], inplace=True) #sort the values
     df.drop(index=df.index[0], axis=0, inplace=True) #remove the smallest value (high chance of being outlier)
     df.drop(index=df.index[len(df.index) - 1], axis=0, inplace=True) #remove the biggest value (high chance of being outlier)
     df.reset_index(drop=True, inplace=True)
-    print(name)
-    print(df)
+    #print(name)
+    #print(df)
 
-#for df, name in zip(dfs_busca, df_names):
-    #dataframeCleaner(df, name) #clean the dataframe
+for dfs in dfs_busca:
+    for df, name in zip(dfs, df_names):
+        dataframeCleaner(df, name)
+
+for dfs in dfs_insercao:
+    for df, name in zip(dfs, df_names):
+        dataframeCleaner(df, name)
+
 
 def med_dsvp(dfs, key):
-    medias_busca = np.array([])
-    dsvp_busca = np.array([])
+    medias = np.array([])
+    dsvp = np.array([])
     for df in dfs:
-        media = df[key].mean()
-        medias_busca = np.append(medias_busca, media)
+        media = df.mean()
+        medias = np.append(medias, media)
 
-        desv_pad = df[key].std()
-        dsvp_busca = np.append(dsvp_busca, desv_pad)
+        desv_pad = df.std()
+        dsvp = np.append(dsvp, desv_pad)
 
-    return medias_busca, dsvp_busca
+    return medias, dsvp
 
-medias_busca_div, dsvp_busca_div = med_dsvp(dfs_busca_div, 'TempoBusca')
-medias_busca_mul, dsvp_busca_mul = med_dsvp(dfs_busca_mul, 'TempoBusca')
-medias_busca_pri, dsvp_busca_pri = med_dsvp(dfs_busca_pri, 'TempoBusca')
+busca_data_points = [[0, 0], [0, 0], [0, 0]]
+insercao_data_points = [[0, 0], [0, 0], [0, 0]]
+
+for i in range(len(dfs_busca)):
+    busca_data_points[i][0], busca_data_points[i][1] = med_dsvp(dfs_busca[i], 'TempoBusca')
+
+for i in range(len(dfs_insercao)):
+    insercao_data_points[i][0], insercao_data_points[i][1] = med_dsvp(dfs_insercao[i], 'TempoInsercao')
+
 
 x = np.arange(len(df_types))
 width = 0.3
-fig, ax = plt.subplots()
-rects1 = ax.errorbar(x - width/2, medias_busca_div, dsvp_busca_div, fmt = 'o', capsize=3, label='Divisão')
-rects2 = ax.errorbar(x, medias_busca_mul, dsvp_busca_mul, fmt = 'o', capsize=3, label='Multiplicação')
-rects3 = ax.errorbar(x + width/2, medias_busca_pri, dsvp_busca_pri, fmt = 'o', capsize=3, label='Primo')
+fig1, ax1 = plt.subplots()
+fig2, ax2 = plt.subplots()
 
-print(rects1)
-print(rects2)
-print(rects3)
+j = -width/2
+for data, name in zip(busca_data_points, df_names):
+    ax1.errorbar(x - j, data[0], data[1], fmt = 'o', capsize=3, label=name)
+    j += width/2
+
+j = -width/2
+for data, name in zip(insercao_data_points, df_names):
+    ax2.errorbar(x - j, data[0], data[1], fmt = 'o', capsize=3, label=name)
+    j += width/2
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
-ax.set_ylabel('Tempo(s)')
-ax.set_title('Tempo de busca hash')
-ax.set_xticks(x);ax.set_xticklabels(df_types, minor=False)
-ax.set_yticks(np.arange(0.000, 0.250, 0.010))
-plt.grid(which='major', axis='y')
-ax.legend()
+ax1.set_ylabel('Tempo(s)')
+ax1.set_title('Tempo de busca hash')
+ax1.set_xticks(x);ax1.set_xticklabels(df_types, minor=False)
+ax1.set_yticks(np.arange(0.000, 0.250, 0.010))
+ax1.legend()
+fig1.tight_layout()
+ax1.grid(which='major', axis='y')
 
-ax.set_xlabel(rects1)
-ax.set_xlabel(rects2)
-ax.set_xlabel(rects3)
-
-fig.tight_layout()
+ax2.set_ylabel('Tempo(s)')
+ax2.set_title('Tempo de inserção hash')
+ax2.set_xticks(x);ax2.set_xticklabels(df_types, minor=False)
+ax2.set_yticks(np.arange(0.000, 0.09, 0.005))
+ax2.legend()
+fig2.tight_layout()
+ax2.grid(which='major', axis='y')
 
 plt.show()
 
@@ -101,24 +135,7 @@ print(busca_media_dsvp_df)
 #busca_media_dsvp_df.to_csv(('out/busca_hash_media_dsvp.csv')) """
 
 
-""" plt.errorbar(combined_df_names, medias_busca, dsvp_busca, linestyle='None', marker='o', capsize=3)
-plt.title('Busca por Espalhamento')
-plt.ylabel('Tempo (s)')
-plt.xlabel('Busca Espalhada')
-plt.yticks(np.arange(0.005, 0.25, 0.02))
-plt.set_xticks(np.arange(len(df_types)), df_types)
-plt.grid(which='major', axis='y')
-
-plt.show() """
-#plt.savefig('out/busca_hash_media_e_desv_pad.png')
-
-#fdf = pd.DataFrame(np.array([means, stds]).T)
-"""media_e_desv_pad_df = pd.DataFrame([means, stds], index=['Média', 'Desvio'], columns=df_names)
-media_e_desv_pad_df = media_e_desv_pad_df.round(decimals=4)
-print(media_e_desv_pad_df)
-media_e_desv_pad_df.to_csv('out/media_e_desv_pad.csv')
-
-all_values_df = pd.DataFrame()
+""" all_values_df = pd.DataFrame()
 
 for df, name in zip(dfs, df_names):
     all_values_df[name] = df['Tempo']
